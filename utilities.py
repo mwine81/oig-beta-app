555d622db26b7d1d6830facd7b2346e96afd8eef
matthewwine <mwine81@gmail.com>
2025-01-12 11:57:03 -0500

    Refactor provider handling and enhance data filtering logic
    Updated provider dropdown to dynamically set options based on product selection. Centralized path handling, improved data filtering, and added constraints for provider groups. Adjusted chart formatting and cleaned up unused code for clarity and efficiency.

--- utilities.py
+++ utilities.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 import polars as pl
 from polars import col as c
 import plotly.express as px
-
 import re
+
+DATA = Path('__main__').parent / 'data/claims_clean.parquet'
 
 interval_dict = {
   'Week':'1w',
@@ -12,8 +14,11 @@ interval_dict = {
 }
 
 
-def get_provider_groups():
-    return pl.scan_parquet('data/claims_clean.parquet').select(c.group_name).filter(c.group_name.is_not_null()).unique().sort('group_name').collect().to_series().to_list()
+def get_product_list() -> list:
+    return pl.scan_parquet(DATA).select(c.product).unique().sort('product').collect().to_series().to_list()
+
+def get_provider_groups(product):
+    return pl.scan_parquet(DATA).filter(c.dos.dt.date() > 2022).filter(c.pbm_id == 1).filter(c.product == product).group_by(c.group_name).len().filter(c.group_name.is_not_null()).filter(c.len > 2).unique().sort('group_name').collect().to_series().to_list()
 
 def get_interval():
     return [i for i in interval_dict]
@@ -25,7 +30,7 @@ icp_unit = (c.icp / c.qty).alias("icp_unit")
 nadac_unit = (c.nadac/c.qty).alias("nadac_unit")
 
 def generate_data_by_period(product, interval,filter_groups):
-    data = pl.scan_parquet('data/claims_clean.parquet').filter(c.dos.dt.date() > 2022).filter(c.pbm_id == 1)
+    data = pl.scan_parquet(DATA).filter(c.dos.dt.date() > 2022).filter(c.pbm_id == 1)
     return(
     data
     .filter(c.fee < 10)
@@ -46,10 +51,6 @@ def generate_data_by_period(product, interval,filter_groups):
     )
     .with_columns(pl.lit(interval).alias('interval'))
     )
-
-# def run(intervals: list = ('1mo','1q','1y','1w')):
-#     pl.concat([generate_data_by_period(i) for i in intervals]).collect().write_parquet(DATA)
-
 
 def load_data(product: str,interval: str,filter_groups:list, qty: float) -> pl.LazyFrame:
     data = generate_data_by_period(product,interval,filter_groups)
@@ -74,7 +75,7 @@ def create_figure(product,interval,filter_groups, qty):
                     line_dash='variable',
                   )
     fig.update_layout(
-        yaxis_tickformat = '$.0f',
+        yaxis_tickformat = '$,.2f',
         title_subtitle_text=f"Express Scripts WV NADAC OIG Reporting 2023-2024 (Average by {interval})",
         plot_bgcolor='white',
         legend_x = .90,
@@ -84,20 +85,22 @@ def create_figure(product,interval,filter_groups, qty):
     fig.update_yaxes(
         ticksuffix = '      ',
         tickfont=dict(
-            family="Arial Black",  # Choose a bold font family
+            family="Arial",  # Choose a bold font family
             size=12,
-            color="grey"
-    ))
+            color="grey"),
+        title_font_family='Arial black'
+    )
     fig.update_xaxes(
         tickfont=dict(
-            family="Arial Black",  # Choose a bold font family
+            family="Arial",  # Choose a bold font family
             size=12,
-            color="grey"
-    ))
+            color="grey"),
+    title_text = 'Interval',
+    title_font_family='Arial black'
+    )
     fig.update_traces(
-        line=dict(width=4),
+        line=dict(width=3),
     )
     return fig
 
-def get_product_list() -> list:
-    return pl.scan_parquet('data/claims_clean.parquet').select(c.product).unique().sort('product').collect().to_series().to_list()
+
