f97244747df52234f70b46595897525e1c6d2c8c
matthewwine <mwine81@gmail.com>
2025-01-18 14:53:16 -0500

    Refactor app layout and enhance data aggregation features
    Introduced reusable components for dropdowns and a revamped app layout for better modularity. Enhanced data aggregation functionality in utilities.py, adding support for filtering by drug type, specialty, and brand/generic status, while improving figure generation with percentage-based metrics.

--- app.py
+++ app.py
@@ -1,90 +1,231 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash import html, dcc,Input, Output, callback
-from utilities import *
+from dash import html, dcc, Input, Output, callback, State
+from utilities import get_product_list, get_interval, create_figure, get_provider_groups, percent_of_total_frame, \
+    percent_total_fig
 
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
+# Constants
+DEFAULT_PRODUCT = 'Atorvastatin Calcium Oral Tablet 20 MG'
+DEFAULT_INTERVAL = get_interval()[2]
+DEFAULT_QUANTITY = 60
 
-product_dd = html.Div([
-            dbc.Label('Product Selection:'),
-            dcc.Dropdown(
-                options=get_product_list(),
-                value='Atorvastatin Calcium Oral Tablet 20 MG',
-                id='product-dropdown',
-                placeholder='Select a product'
-            )])
 
-interval_dd = html.Div([
-            dbc.Label('Interval Selection:'),
-            dcc.Dropdown(
-                options=get_interval(),
-                value=get_interval()[2],
-                id='interval-dropdown',
-            )])
-provider_dd =html.Div( [
-            dbc.Label('Chose Providers to add:'),
-            dcc.Dropdown(value=[],multi=True,id='provider-dropdown')
-        ])
-
-quantity_selection = html.Div([
-    dbc.Label('Quantity Selection:'),
-    dcc.Dropdown([x for x in range(1000)],
-                value=60,
-                id='input-qty',)
+# Reusable function for dropdown cards
+def create_dropdown_card(label, dropdown_id, options=[], default_value=None, multi=False, placeholder=None):
+    return html.Div([
+        dbc.Label(label, className="fw-bold"),
+        dcc.Dropdown(
+            options=options,
+            value=default_value,
+            id=dropdown_id,
+            multi=multi,
+            placeholder=placeholder,
+        ),
     ])
 
-controls = dbc.Card([
-    html.Div(product_dd),
-    html.Div(interval_dd),
-    html.Div(provider_dd),
-    html.Div(quantity_selection)],
-    body=True,
+
+# Components
+product_dropdown = create_dropdown_card(
+    label="Product Selection:",
+    dropdown_id="product-dropdown",
+    options=get_product_list(),
+    default_value=DEFAULT_PRODUCT,
+    placeholder="Select a product"
 )
 
+interval_dropdown = create_dropdown_card(
+    label="Interval Selection:",
+    dropdown_id="interval-dropdown",
+    options=get_interval(),
+    default_value=DEFAULT_INTERVAL,
+)
+
+provider_dropdown = create_dropdown_card(
+    label="Choose Providers to Add:",
+    dropdown_id="provider-dropdown",
+    default_value=[],
+    multi=True,
+)
+
+quantity_dropdown = create_dropdown_card(
+    label="Quantity Selection:",
+    dropdown_id="input-qty",
+    options=[{"label": x, "value": x} for x in range(1000)],
+    default_value=DEFAULT_QUANTITY,
+)
 
 controls = dbc.Card(
-    [provider_dd,interval_dd,product_dd,quantity_selection],
+    [product_dropdown, interval_dropdown, provider_dropdown, quantity_dropdown],
     body=True,
 )
 
-fig_layout = dbc.Card(dcc.Graph(id='main-graph'), className="mt-3")
+fig_card = dbc.Card(dcc.Graph(id="main-graph"), className="mt-3")
 
-
-app.layout = dbc.Container([
-    html.Div([
-    dbc.Row([
-    dbc.Col(html.H1('WV Commercial Claims Data',className="text-white p-3 rounded mb-4", style={'background-color':'#091851'}),width=9),
-        dbc.Col(html.Img(src='assets/final logo (1).png'), width=3,className='p-3')
-    ],justify="between"),
-    dbc.Row([
-        dbc.Col(controls)
-    ]),
-    dbc.Row([
-        dbc.Col([fig_layout])
-    ]),
-    ],className = 'bg-light p-4 rounded')
-],className="p-4",fluid=True)
-
-@callback(
-    Output('main-graph', 'figure'),
-    Input('product-dropdown', 'value'),
-    Input('interval-dropdown', 'value'),
-    Input('provider-dropdown', 'value'),
-    Input('input-qty', 'value')
+agg_radio_group = html.Div(
+    [
+        dbc.RadioItems(
+            id="agg-type",
+            className="btn-group",
+            inputClassName="btn-check",
+            labelClassName="btn btn-outline-primary",
+            labelCheckedClassName="active",
+            options=[{"label": "All Drugs", "value": "All Drugs"},
+                     {"label": "Selected Drug", "value": "Selected Drug"}],
+            value="All Drugs",
+        ),
+        html.Div(id="output_agg-type"),
+    ],
+    className="radio-group",
 )
-def update_fig(product,interval,provider,qty):
-    fig = create_figure(product=product,interval=interval,filter_groups=provider,qty=qty)
-    return fig
+
+specialty_radio_group = html.Div(
+    [
+        dbc.RadioItems(
+            id="specialty",
+            className="btn-group",
+            inputClassName="btn-check",
+            labelClassName="btn btn-outline-primary",
+            labelCheckedClassName="active",
+            options=[
+                {"label": "All", "value": "All"},
+                {"label": "Specialty", "value": "Specialty"},
+                {"label": "Non Specialty", "value": "Non Specialty"},
+            ],
+            value="All",
+        ),
+        html.Div(id="output_specialty"),
+    ],
+    className="radio-group"
+)
+
+b_g_radio_group = html.Div(
+    [
+        dbc.RadioItems(
+            id="b_g",
+            className="btn-group",
+            inputClassName="btn-check",
+            labelClassName="btn btn-outline-primary",
+            labelCheckedClassName="active",
+            options=[
+                {"label": "All", "value": "All"},
+                {"label": "Brand", "value": 1},
+                {"label": "Generic", "value": 0},
+            ],
+            value="All",
+        ),
+        html.Div(id="output_b_g"),
+    ],
+    className="radio-group"
+)
+
+agg_controls = dbc.Card(
+    [
+        dbc.Label("Aggregate Analysis:", className="fw-bold"),
+        agg_radio_group,
+        dbc.Label("Specialty Selection:", className="fw-bold"),
+        specialty_radio_group,
+        dbc.Label("Brand/Generic:", className="fw-bold"),
+        b_g_radio_group,
+    ],className='p-2'
+)
+
+collapse = html.Div(
+    [
+        dbc.Button(
+            "Open collapse",
+            id="horizontal-collapse-button",
+            className="mb-3",
+            color="primary",
+            n_clicks=0,
+        ),
+        html.Div(
+            dbc.Collapse(
+                dbc.Card(
+                    dbc.CardBody(
+                        "This content appeared horizontally due to the "
+                        "`dimension` attribute"
+                    ),
+                    style={"width": "400px"},
+                ),
+                id="horizontal-collapse",
+                is_open=False,
+                dimension="width",
+            ),
+            style={"minHeight": "100px"},
+        ),
+    ]
+)
+
+
+
+
+agg_fig_card = dbc.Card(dcc.Graph(id="agg-analysis"))
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+# Layout
+app.layout = dbc.Container(
+    [
+        html.Div(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(html.H1("WV Commercial Claims Data", className="text-white p-3 rounded mb-4",
+                                        style={"backgroundColor": "#091851"}), width=9),
+                        dbc.Col(html.Img(src="assets/final logo (1).png"), width=3, className="p-3"),
+                    ],
+                    justify="between",
+                ),
+                dbc.Row([dbc.Col(controls)]),
+                dbc.Row([dbc.Col(fig_card)]),
+                dbc.Row(
+                    [
+                        dbc.Col(agg_controls, width=4),
+                        dbc.Col(agg_fig_card, width=8),
+                    ],
+                    className="mt-4 bg-light",
+                ),
+            ],
+            className="bg-light p-3 rounded",
+        )
+    ],
+    className="p-4",
+    fluid=True,
+)
+
+
+# Callbacks
+@callback(
+    Output("main-graph", "figure"),
+    Input("product-dropdown", "value"),
+    Input("interval-dropdown", "value"),
+    Input("provider-dropdown", "value"),
+    Input("input-qty", "value"),
+)
+def update_fig(product, interval, provider, qty):
+    return create_figure(product=product, interval=interval, filter_groups=provider, qty=qty)
+
 
 @app.callback(
-    Output('provider-dropdown', "options"),
-    Input('product-dropdown', "value"),
+    Output("provider-dropdown", "options"),
+    Input("product-dropdown", "value"),
 )
-def set_provider_options(product):
-    return (
-        get_provider_groups(product)
-    )
+def update_provider_options(product):
+    return get_provider_groups(product)
+
+
+@app.callback(
+    Output("agg-analysis", "figure"),
+    Input("agg-type", "value"),
+    Input("product-dropdown", "value"),
+    Input("specialty", "value"),
+    Input("b_g", "value"),
+)
+def update_agg_fig(use_drug, product, specialty_selection,is_brand):
+    data = percent_of_total_frame(use_drug, product, specialty_selection,is_brand)
+    return percent_total_fig(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utilities.py
+++ utilities.py
@@ -12,12 +12,11 @@ interval_dict = {
    'Year':'1y'
 }
 
-
 def get_product_list() -> list:
     return pl.scan_parquet(DATA).select(c.product).unique().sort('product').collect().to_series().to_list()
 
 def get_provider_groups(product):
-    return pl.scan_parquet(DATA).filter(c.dos.dt.date() > 2022).filter(c.pbm_id == 1).filter(c.product == product).group_by(c.group_name).len().filter(c.group_name.is_not_null()).filter(c.len > 2).unique().sort('group_name').collect().to_series().to_list()
+    return pl.scan_parquet(DATA).filter(c.product == product).group_by(c.group_name).len().filter(c.group_name.is_not_null()).filter(c.len > 2).unique().sort('group_name').collect().to_series().to_list()
 
 def get_interval():
     return [i for i in interval_dict]
@@ -28,11 +27,11 @@ def filter_group(group):
 icp_unit = (c.icp / c.qty).alias("icp_unit")
 nadac_unit = (c.nadac/c.qty).alias("nadac_unit")
 
+
 def generate_data_by_period(product, interval,filter_groups):
-    data = pl.scan_parquet(DATA).filter(c.dos.dt.date() > 2022).filter(c.pbm_id == 1)
+    data = pl.scan_parquet(DATA)
     return(
     data
-    .filter(c.fee < 10)
     .filter(c.product == product)
     .with_columns(icp_unit,nadac_unit)
     .sort(by="dos")
@@ -67,8 +66,6 @@ def create_figure(product,interval,filter_groups, qty):
                   y='value',
                   color='variable',
                   line_shape='spline',
-                  # width=1200,
-                  # height=600,
                   labels={'dos':'','value':'Avg ICP per Rx','variable':''},
                   title=f"Average Ingredient Cost per Rx for <br><b>{product}<b> x {qty} ct",
                     line_dash='variable',
@@ -105,6 +102,84 @@ def create_figure(product,interval,filter_groups, qty):
     fig.update_traces(
         line=dict(width=3),
 
+    )
+    return fig
+
+CATEGORY_ORDER = {
+    'variable': ['Rx Dispensed', 'Units Dispensed', 'Ingredient Cost Paid', 'NADAC', 'Margin Over NADAC']
+}
+REPLACEMENT_MAP = {
+    'qty': "Units Dispensed",
+    'rx_count': "Rx Dispensed",
+    'icp': "Ingredient Cost Paid",
+    'nadac': "NADAC",
+    'margin': "Margin Over NADAC"
+}
+AFFILIATED_MAP = {'true': 'Affiliated', 'false': 'Non-Affiliated'}
+COLOR_MAP = {'Affiliated': '#0077BE', 'Non-Affiliated': '#091851'}
+
+
+# Helper function: Apply aggregations
+def apply_aggregation(data):
+    return data.group_by(c.affiliated).agg(
+        pl.len().alias('rx_count'),
+        c.nadac.sum(),
+        c.icp.sum(),
+        c.qty.sum()
+    )
+
+# Helper function: Apply column calculations
+def apply_calculation(data):
+    return data.with_columns(
+        (c.icp - c.nadac).alias('margin')
+    ).unpivot(index='affiliated').with_columns(
+        (c.value / c.value.sum().over('variable')).alias('percent'),
+        c.variable.replace(REPLACEMENT_MAP),
+        c.affiliated.cast(pl.String).replace(AFFILIATED_MAP)
+    ).sort(c.affiliated)
+
+
+# Main function: Percent of total frame
+def percent_of_total_frame(use_drug, product,specialty_selection,b_g):
+    scanned_data = pl.scan_parquet(DATA)
+    if use_drug == 'Selected Drug':
+        scanned_data = scanned_data.filter(c.product == product)
+    if specialty_selection == 'Specialty':
+        scanned_data = scanned_data.filter(c.is_special)
+    if specialty_selection == 'Non-Specialty':
+        scanned_data = scanned_data.filter(c.is_special == False)
+    if b_g == 1:
+        scanned_data = scanned_data.filter(c.is_brand == True)
+    if b_g == 0:
+        scanned_data = scanned_data.filter(c.is_brand == False)
+
+    aggregated_data = apply_aggregation(scanned_data)  # Apply aggregation
+    return apply_calculation(aggregated_data).collect()  # Perform calculations
+
+
+# Refactored function: Percent total figure
+def percent_total_fig(data):
+    fig = px.bar(
+        data,
+        x='percent',
+        y='variable',
+        color='affiliated',
+        barmode='relative',
+        text='value',
+        title='Affiliated vs Non-Affiliated',
+        color_discrete_map=COLOR_MAP,
+        category_orders=CATEGORY_ORDER
+    )
+    fig.update_traces(
+        textposition='inside',
+        texttemplate='%{x:.0%}',
+        textfont_size=13,
+        width=0.75
+    )
+    fig.update_xaxes(showticklabels=False, title_text='')
+    fig.update_yaxes(title_text='(Percent of Total)', ticksuffix=' ')
+    fig.update_layout(
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, x=0, title_text="")
     )
     return fig
 
